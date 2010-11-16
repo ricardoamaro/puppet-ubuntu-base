45ed4374420781281fae1a56e0abb747a87f6031
Alexander Werner <bundeswerner@googlemail.com>
2010-11-16 02:24:00 +0100

    Updated init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,124 +4,195 @@
 # Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
 # See LICENSE for the full license granted to you.
 #
-# Adapted by Alexander Werner <bundeswerner@googlemail.com>
+# Adapted by Alexander Werner <alex@documentfoundation.org>
 
 class ubuntu-base {
+  case $operatingsystem {
+    'Ubuntu': { }
+    default: { fail("\$operatingsystem of ${fqdn} is not 'Ubuntu' but '${operatingsystem}'.") }
+  }
+  
+  #######################################################################
+  #######################################################################
+  # Basic package selection
+  package {
+    ["apticron",
+     "apt-listchanges",
+     "deborphan",
+     "screen",
+     "mmv",
+     "less",
+     "bzip2",
+     "net-tools",
+     "man-db",
+     "sudo",
+     "pwgen",
+     "reportbug",
+     "pciutils",
+     "bash-completion",
+     "anacron",
+     "dnsutils",
+     "procmail",
+     "iptraf",
+     "lynx",
+     "ftp",
+     "nmap",
+     "bsd-mailx",
+     "zip",
+     "mc",
+     "telnet",
+     "patch",
+     "at",
+     "lha",
+     "arj",
+     "unrar",
+     "zoo",
+     "nomarch",
+     "lzop",
+     "cabextract",
+     "p7zip-full",
+     "rpm",
+     "pax",
+     "alien",
+     "htop",
+     "lsof",
+     "tofrodos",
+     "recode",
+     "fetchmail",
+     "ca-certificates",
+     "mutt",
+     "vacation",
+     "bc",
+     "manpages-de-dev",
+     "lftp",
+     "python",
+     "tcpdump",
+     "w3m",
+     "unzip",
+     "strace",
+     "subversion",
+     "cvs",
+     "openbsd-inetd",
+     "iproute",
+     "etherwake",
+     "smbclient",
+     "smbfs",
+     "geoip-bin",
+     "scponly",
+     "rar",
+     "git-core",
+     "landscape-common",
+     "openssh-blacklist",
+     "openssh-blacklist-extra",
+     "openssl-blacklist",
+     "openssl-blacklist-extra",
+     "openvpn-blacklist",
+     "traceroute",
+     "whois",
+     "chkrootkit",
+     "rkhunter",
+     "curl",
+     "ethstatus",
+     "nload",
+     "iftop",
+     "joe",
+     "sysstat",
+     "apparmor-profiles"
+     ]:
+       ensure => installed;
+     
+  }
+  
+  #######################################################################
+  #######################################################################
+  # apt-listchanges
+  file {
+    "/etc/apt/listchanges.conf":
+      mode => 0644, owner => root, group => root,
+      before => Package[apt-listchanges],
+      source => "puppet:///modules/ubuntu-base/apt-listchanges.conf";
 
-	case $operatingsystem {
-		'Ubuntu': { }
-		default: { fail("\$operatingsystem of ${fqdn} is not 'Ubuntu' but '${operatingsystem}'.") }
-	}
-
-	# Save space here: vservers don't need old packages around, there will
-	# be always network around to fix them
-	$apt_clean = $vserver ? { 
-		'guest' => 'always',
-		default => 'auto',
-	}
-
-	#######################################################################
-	#######################################################################
-
-	package {
-		[
-			apticron,
-			apt-listchanges,
-			screen, mmv, deborphan, less, iproute, bzip2,
-			net-tools, man-db,
-			sudo, pwgen,
-			reportbug,
-			pciutils,
-			bash-completion
-		]:
-			ensure => installed;
-		debconf-english:
-			ensure => installed, before => Package[debconf-i18n];
-		debconf-i18n:
-			ensure => purged;
-		[
-			lpr, ppp, pppconfig, pppoe, pppoeconf,
-			tasksel, tasksel-data, installation-report,
-			dmidecode, laptop-detect, whiptail, info,
-			pidentd, pump, nfs-common, nfs-kernel-server, nagios-nrpe-server
-		]:
-			ensure => purged;
-	}
-
-	#######################################################################
-	#######################################################################
-
-	# various config files
-	file {
-		"/etc/apt/listchanges.conf":
-			mode => 0644, owner => root, group => root,
-			#before => [ File[apt_config], Package[apt-listchanges] ],
-			before => Package[apt-listchanges],
-			source => "puppet:///modules/ubuntu-base/apt-listchanges.conf";
-		#"/etc/hosts":
-		#	mode => 0644, owner => root, group => root,
-		#	source => "puppet:///modules/dbp/hosts";
-	}
-	
-	#######################################################################
-	#######################################################################
-
-	package { locales: ensure => installed }
-	file {
-		"/etc/default/locale":
-			mode => 0644, owner => root, group => root,
-			source => "puppet:///modules/ubuntu-base/default_locale";
-		"/etc/locale.gen":
-			mode => 0644, owner => root, group => root,
-			source => "puppet:///modules/ubuntu-base/locale.gen";
-	}
-	exec { "/usr/sbin/locale-gen":
-		refreshonly => true,
-		require => Package[locales],
-		subscribe => File["/etc/locale.gen"]
-	}
-
-	#######################################################################
-	#######################################################################
-
-	#puppet::munin::resources { $fqdn: }
-
-	#######################################################################
-	#######################################################################
-
-	# remove all local bucketed files after a month.
-	tidy { '/var/lib/puppet/clientbucket':
-		backup => false,
-		recurse => true,
-		rmdirs => true,
-		type => mtime,
-		age => '4w',
-	}
-	
-	#######################################################################
-	#######################################################################
-
-	package { ["libpam-cracklib", "wamerican", "wamerican-huge",
-		   "wamerican-large", "wamerican-small", "wbrazilian",
-		   "wbritish", "wbritish-huge", "wbritish-large",
-		   "wbritish-small", "wbulgarian", "wcanadian",
-		   "wcanadian-huge", "wcanadian-large", "wcanadian-small",
-		   "wcatalan", "wdanish", "wdutch", "wfaroese", "wfinnish",
-		   "wfrench", "wgaelic", "wgalician-minimos", "wirish",
-		   "witalian", "wmanx", "wngerman", "wnorwegian", "wogerman",
-		   "wpolish", "wportuguese", "wspanish", "wswedish", "wswiss",
-		   "wukrainian"]:
-		ensure => installed
-	}
-	exec { "/etc/cron.daily/cracklib-runtime":
-		refreshonly => true
-	}
-
-        ######################################################################
-        ######################################################################
-
-        package {["emacs23-nox", "puppet-el", "emacs-goodies-el"]:
-          ensure => installed,
-        }
-
+  }
+  
+  #######################################################################
+  #######################################################################
+  # Locales
+  package { locales: ensure => installed }
+  file {
+    "/etc/default/locale":
+      mode => 0644, owner => root, group => root,
+      source => "puppet:///modules/ubuntu-base/default_locale";
+    "/etc/locale.gen":
+      mode => 0644, owner => root, group => root,
+      source => "puppet:///modules/ubuntu-base/locale.gen";
+  }
+  exec { "/usr/sbin/locale-gen":
+    refreshonly => true,
+    require => Package[locales],
+    subscribe => File["/etc/locale.gen"]
+  }
+  
+  #######################################################################
+  #######################################################################
+  # remove all local bucketed files after a month.
+  tidy { '/var/lib/puppet/clientbucket':
+    backup => false,
+    recurse => true,
+    rmdirs => true,
+    type => mtime,
+    age => '4w',
+  }
+  
+  #######################################################################
+  #######################################################################
+  # Cracklib
+  package {
+    ["libpam-cracklib",
+     "wamerican",
+     "wamerican-huge",     
+     "wamerican-large",
+     "wamerican-small",
+     "wbrazilian",
+     "wbritish",
+     "wbritish-huge",
+     "wbritish-large",
+     "wbritish-small",
+     "wbulgarian",
+     "wcanadian",
+     "wcanadian-huge",
+     "wcanadian-large",
+     "wcanadian-small",
+     "wcatalan",
+     "wdanish",
+     "wdutch",
+     "wfaroese",
+     "wfinnish",
+     "wfrench",
+     "wgaelic",
+     "wgalician-minimos",
+     "wirish",
+     "witalian",
+     "wmanx",
+     "wngerman",
+     "wnorwegian",
+     "wogerman",
+     "wpolish",
+     "wportuguese",
+     "wspanish",
+     "wswedish",
+     "wswiss",
+     "wukrainian"
+     ]:
+	       ensure => installed
+  }
+  exec { "/etc/cron.daily/cracklib-runtime":
+    refreshonly => true
+  }
+  
+  ######################################################################
+  ######################################################################
+  # Emacs
+  package {["emacs23-nox", "puppet-el", "emacs-goodies-el"]:
+    ensure => installed,
+  }  
 }
